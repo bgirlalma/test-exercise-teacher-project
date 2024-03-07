b8e3c5decb480156a362ff163e9207db7549515b
Alina <akulina.alina1999gmail.com>
2024-03-07 22:17:54 +0300

    add localstorage

--- react-project/src/components/teachers/teacher-items/info/info-teacher.jsx
+++ react-project/src/components/teachers/teacher-items/info/info-teacher.jsx
@@ -12,30 +12,53 @@ import {
 import { HeartYellowSvg } from '../../../image/heartYellow';
 import { useDispatch} from 'react-redux';
 import {addTeacherFavorites, removeTeacherFavorites} from '../../../../redux/teacher/teacherOperation'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
  
 
 const InfoTeacher = ({ teacher }) => {
   const dispatch = useDispatch()
   const [isFavorite, setIsFavorite] = useState(false);
-  
 
-  const addToFavoritesHandler = () => {
-    console.log('Trying to add teacher to favorites:', teacher.id);
+  // перевірка state
+  useEffect(() => {
+    const favorites = JSON.parse(localStorage.getItem('favorites'))
+    if (favorites && favorites.includes(teacher.id)) {
+      setIsFavorite(true)
+    }
+  }, [teacher.id])
+
+  const updateFavorites = () => {
     if (!isFavorite) {
       console.log('Adding teacher to favorites:', teacher.id);
       dispatch(addTeacherFavorites(teacher.id));
-      setIsFavorite(true)
+      setIsFavorite(true);
+      const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+      localStorage.setItem('favorites', JSON.stringify([...favorites, teacher.id]))
     } else {
-      console.log('Teacher is already in favorites:', teacher.id);
+       dispatch(removeTeacherFavorites(teacher.id));
+      setIsFavorite(false);
+      const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+      localStorage.setItem('favorites', JSON.stringify(favorites.filter(id => id !== teacher.id)))
     }
-  };
+  }
+  
 
-  const removeFromFavoritesHandler = () => {
-    console.log('Trying to remove teacher from favorites:', teacher.id);
-    dispatch(removeTeacherFavorites(teacher.id));
-    setIsFavorite(false)
-  };
+  // const addToFavoritesHandler = () => {
+  //   console.log('Trying to add teacher to favorites:', teacher.id);
+  //   if (!isFavorite) {
+  //     console.log('Adding teacher to favorites:', teacher.id);
+  //     dispatch(addTeacherFavorites(teacher.id));
+  //     setIsFavorite(true)
+  //   } else {
+  //     console.log('Teacher is already in favorites:', teacher.id);
+  //   }
+  // };
+
+  // const removeFromFavoritesHandler = () => {
+  //   console.log('Trying to remove teacher from favorites:', teacher.id);
+  //   dispatch(removeTeacherFavorites(teacher.id));
+  //   setIsFavorite(false)
+  // };
 
   return (
     <Container>
@@ -55,17 +78,15 @@ const InfoTeacher = ({ teacher }) => {
         <Title>Price / 1 hour: {teacher.price_per_hour}</Title>
       </Wrapp>
 
-      {isFavorite ? (
-        <HeartYellowContainer onClick={removeFromFavoritesHandler}>
-          <HeartYellowSvg />
-        </HeartYellowContainer>
-      ) : (
-        <HeartContainer onClick={addToFavoritesHandler}>
-          <HeartSvg />
-        </HeartContainer>
-      )}
+      <HeartYellowContainer onClick={updateFavorites}>
+        {isFavorite ? <HeartYellowSvg /> : <HeartSvg />}
+      </HeartYellowContainer>
     </Container>
   );
 };
+
+  // <HeartContainer>
+  //   <HeartSvg />
+  // </HeartContainer>;
 
 export default InfoTeacher;
