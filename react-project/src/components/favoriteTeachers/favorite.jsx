625ec698a42b8cb2a27f278cd48bc442e2451560
Alina <akulina.alina1999gmail.com>
2024-03-10 21:36:42 +0300

    fix

--- react-project/src/components/favoriteTeachers/favorite.jsx
+++ react-project/src/components/favoriteTeachers/favorite.jsx
@@ -1,28 +1,20 @@
-import { useSelector } from "react-redux";
+
 
 const FavoriteTeachers = () => {
-  const favoritesIds = JSON.parse(localStorage.getItem('favorites')) || [];
-  console.log("Favorites", favoritesIds)
-  const teachers = useSelector(state => state.teachers.teachers);
-  console.log('teachers', teachers)
 
-  const favoritesTeachers = teachers.filter(teacher => favoritesIds.includes(teacher.id))
-  console.log('favorites', favoritesTeachers);
 
   return (
     <div>
       <h2>Favorite Page</h2>
       <ul>
-        {favoritesTeachers.map(teacher => (
-          <li key={teacher.id}>
-            <h2>{teacher.name}</h2>
+   
+          <li>
+            <h2></h2>
             <div>
-              <div>
-
-              </div>
+              <div></div>
             </div>
           </li>
-        ))}
+
       </ul>
     </div>
   );
--- react-project/src/components/teachers/teacher-items/info/info-teacher.jsx
+++ react-project/src/components/teachers/teacher-items/info/info-teacher.jsx
@@ -10,30 +10,46 @@ import {
   HeartYellowContainer,
 } from './info-teacher.styled';
 import { HeartYellowSvg } from '../../../image/heartYellow';
-import { useDispatch, useSelector} from 'react-redux';
-import {addTeacherFavorites, removeTeacherFavorites} from '../../../../redux/teacher/teacherOperation'
-import { useCallback, useEffect, useState } from 'react';
- 
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  addTeacherFavorites,
+  removeTeacherFavorites,
+} from '../../../../redux/teacher/teacherOperation';
 
 const InfoTeacher = ({ teacher }) => {
-  const dispatch = useDispatch()
- const favorites = useSelector(state => state.favorites.favorites);
- const [isFavorite, setIsFavorite] = useState(favorites.includes(teacher.id));
+  const dispatch = useDispatch();
+  const [favoritesTeachers, setFavoritesTeachers] = useState([]);
+  const [isFavorite, setIsFavorite] = useState(false);
 
   useEffect(() => {
-    setIsFavorite(favorites.includes(teacher.id));
-  }, [favorites, teacher.id]);
-
-  const updateFavorites = () => {
-    if (!isFavorite) {
-      console.log('Adding teacher to favorites:', teacher.id);
-      dispatch(addTeacherFavorites(teacher.id));
-    } else {
-      dispatch(removeTeacherFavorites(teacher.id));
+    const favorites = JSON.parse(localStorage.getItem('favorites'));
+    if (favorites && favorites.includes(teacher.id)) {
+      setIsFavorite(true);
     }
-    setIsFavorite(prevIsFavorite => !prevIsFavorite); // Обновляем состояние по колбэку
-  };
-  
+    setFavoritesTeachers(favorites || []);
+  }, [teacher.id]);
+
+    const handleFavorite = () => {
+      if (!isFavorite) {
+        dispatch(addTeacherFavorites(teacher.id));
+        setIsFavorite(true);
+   
+        setFavoritesTeachers(prevFavorites => [...prevFavorites, teacher.id]);
+        localStorage.setItem('favorites', JSON.stringify([...favoritesTeachers, teacher.id]));
+      } else {
+        dispatch(removeTeacherFavorites(teacher.id));
+        setIsFavorite(false);
+
+        setFavoritesTeachers(prevFavorites =>
+          prevFavorites.filter(id => id !== teacher.id)
+        );
+        localStorage.setItem(
+          'favorites',
+          JSON.stringify(favoritesTeachers.filter(id => id !== teacher.id))
+        );
+      }
+    };
 
   return (
     <Container>
@@ -53,7 +69,7 @@ const InfoTeacher = ({ teacher }) => {
         <Title>Price / 1 hour: {teacher.price_per_hour}</Title>
       </Wrapp>
 
-      <HeartYellowContainer onClick={updateFavorites}>
+      <HeartYellowContainer onClick={handleFavorite}>
         {isFavorite ? <HeartYellowSvg /> : <HeartSvg />}
       </HeartYellowContainer>
     </Container>
